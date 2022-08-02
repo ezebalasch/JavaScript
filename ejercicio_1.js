228449219d9846ad55fe485d15320448dd5d7186
Ezequiel Balasch <ezebalasch@gmail.com>
2022-08-01 22:06:59 -0300

    JS 1°dia

---
+++ ejercicio_1.js
@@ -0,0 +1,3 @@
+var clima = prompt("Cómo está el día de hoy?");
+
+alert(`El día de hoy está ${clima}`)
---
+++ ejercicio_2.js
@@ -0,0 +1,5 @@
+var radio = prompt("Ingrese el radio de una circunferencia")
+var pi = Math.PI
+alert(
+    `El radio de una circunferencia es ${(pi*(radio)**2)}
+    Y su perímetro es ${(2*pi*radio)}`)
